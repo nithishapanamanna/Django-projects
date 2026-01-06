ab72c3a0d8c1cac7c087da31aff5319485b9344b
Nithisha <nithishapanamanna@gmail.com>
2026-01-06 17:00:55 +0530

    second commit

--- Ecom/Ecom/wsgi.py
+++ Ecom/Ecom/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path = '/home/NithishaWeb/Ecom'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecom.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
